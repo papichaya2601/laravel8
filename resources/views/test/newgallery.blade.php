f4917fa53ad71ddc1594f79db99c4bfe045697b8
yeepoon <ppapichaya0126@gmail.com>
2021-07-16 12:18:07 +0700

    push to heroku

--- resources/views/test/newgallery.blade.php
+++ resources/views/test/newgallery.blade.php
@@ -7,7 +7,7 @@ img {
 }
 </style>
 <h1>"Gallery of 62122420010 นางสาวปพิชญา ลาดหนองขุ่น"</h1>
-ant <a href="newgallery/ant">
+ant <a href="gallery/ant">
 <img src="{{$ant}}"></a>
 bird <img src="{{$bird}}">
 cat <img src="{{$cat}}">
--- routes/web.php
+++ routes/web.php
@@ -78,3 +78,14 @@ Route::get("/student" , function (){
 Route::get('/table', function () {
     return view('table');
 });
+
+use App\Http\Controllers\MyProfileController;
+
+Route::get("/myprofile/create",[ MyProfileController::class , "create" ]);
+Route::get("/myprofile/{id}/edit", [ MyProfileController::class , "edit" ] );
+Route::get("/myprofile/{id}", [ MyProfileController::class , "show" ]);
+
+Route::get( "/newgallery" , [ MyProfileController::class , "gallery" ] );
+Route::get( "/newgallery/ant" , [ MyProfileController::class , "ant" ] );
+
+
