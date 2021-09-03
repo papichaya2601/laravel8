6e3a6b237eb89823df314fdbc0b564359101a85b
yeepoon <ppapichaya0126@gmail.com>
2021-09-03 11:37:45 +0700

    week10

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StaffController;
+use App\Http\Controllers\MyProfileController;
+use App\Http\Controllers\Covid19Controller;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,41 +21,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get("/homepage", function() {
-    return "<h1>This is home page</h1>" ;
+Route::get("/homepage", function () {
+    return "<h1>This is home page</h1>";
 });
-Route::get("/blog/{id}", function($id) {
-    return "<h1>This is blog page : {$id} </h1>" ;
+Route::get("/blog/{id}", function ($id) {
+    return "<h1>This is blog page : {$id} </h1>";
 });
-Route::get( "/blog/{id}/edit" , function($id) {
-    return "<h1>This is blog page : {$id} for edit</h1>" ;
+Route::get("/blog/{id}/edit", function ($id) {
+    return "<h1>This is blog page : {$id} for edit</h1>";
 });
-Route::get("/product/{a}/{b}/{c}", function($a, $b, $c) {
-    return "<h1>This is product page </h1><div>{$a} , {$b}, {$c}</div>" ;
+Route::get("/product/{a}/{b}/{c}", function ($a, $b, $c) {
+    return "<h1>This is product page </h1><div>{$a} , {$b}, {$c}</div>";
 });
-Route::get("/hello", function () {	
+Route::get("/hello", function () {
     return view("hello");
-    });
-    
+});
+
+
 Route::get('/greeting', function () {
 
-	$name = 'James';
-$last_name = 'Mars';
+    $name = 'James';
+    $last_name = 'Mars';
 
-return view('greeting', compact('name','last_name') );
+    return view('greeting', compact('name', 'last_name'));
 });
-Route::get( "/gallery" , function(){
-$ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-$bird = "https://www.hebergementwebs.com/image/cc/cc8811773d2cdbeb4d46e5550fc455fe.jpg/falcon-and-the-winter-soldier-falcon-minifigure-captain-america.jpg";
-$cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
-$god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg";
-$spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg"; 
-
-return view("test/index", compact("ant","bird","cat","god","spider") );
-});
-Route::get( "/gallery/ant" , function(){
+Route::get("/gallery", function () {
     $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-return view("test/ant", compact("ant"));
+    $bird = "https://www.hebergementwebs.com/image/cc/cc8811773d2cdbeb4d46e5550fc455fe.jpg/falcon-and-the-winter-soldier-falcon-minifigure-captain-america.jpg";
+    $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
+    $god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg";
+    $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg";
+
+    return view("test/index", compact("ant", "bird", "cat", "god", "spider"));
+});
+Route::get("/gallery/ant", function () {
+    $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
+    return view("test/ant", compact("ant"));
 });
 
 
@@ -58,14 +64,15 @@ Route::middleware(['auth', 'role:admin,teacher'])->group(function () {
     Route::get('/teacher', function () {
         return view('teacher/index');
     });
-    
+});
 
-Route::get("/student" , function (){
-	return view("student");
+Route::get("/student", function () {
+    return view("student");
 });
 
 
- 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -76,74 +83,45 @@ Route::get("/student" , function (){
 | contains the "web" middleware group. Now create something great!
 |
 */
- 
+
 Route::get('/table', function () {
     return view('table');
 });
 
-use App\Http\Controllers\MyProfileController;
-
-Route::get("/myprofile/create",[ MyProfileController::class , "create" ]);
-Route::get("/myprofile/{id}/edit", [ MyProfileController::class , "edit" ] );
-Route::get("/myprofile/{id}", [ MyProfileController::class , "show" ]);
-
-Route::get( "/newgallery" , [ MyProfileController::class , "gallery" ] );
-Route::get( "/newgallery/ant" , [ MyProfileController::class , "ant" ] );
-
-Route::get( "/coronavirus" ,[ MyProfileController::class , "coronavirus" ] );
 
 
-use App\Http\Controllers\Covid19Controller;
-Route::get('/covid19', [ Covid19Controller::class,"index" ]);
+Route::get("/myprofile/create", [MyProfileController::class, "create"]);
+Route::get("/myprofile/{id}/edit", [MyProfileController::class, "edit"]);
+Route::get("/myprofile/{id}", [MyProfileController::class, "show"]);
 
-use App\Http\Controllers\Covid19Controller;
-Route::get('/covid19', [ Covid19Controller::class,"index" ]);
+Route::get("/newgallery", [MyProfileController::class, "gallery"]);
+Route::get("/newgallery/ant", [MyProfileController::class, "ant"]);
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get("/homepage", function() {
-    return "<h1>This is home page</h1>" ;
-});
-Route::get("/blog/{id}", function($id) {
-    return "<h1>This is blog page : {$id} </h1>" ;
-});
-Route::get( "/blog/{id}/edit" , function($id) {
-    return "<h1>This is blog page : {$id} for edit</h1>" ;
-});
-Route::get("/product/{a}/{b}/{c}", function($a, $b, $c) {
-    return "<h1>This is product page </h1><div>{$a} , {$b}, {$c}</div>" ;
-});
-Route::get("/hello", function () {	
-    return view("hello");
-    });
+Route::get("/coronavirus", [MyProfileController::class, "coronavirus"]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get("/covid19/create",[ Covid19Controller::class , "create" ]);
+require __DIR__ . '/auth.php';
 
-Route::get("/covid19/{id}/edit", [ Covid19Controller::class , "edit" ]);
 
-Route::get('/covid19', [ Covid19Controller::class, 'index' ]);
+Route::get("/covid19/create", [Covid19Controller::class, "create"]);
+Route::get("/covid19/{id}/edit", [Covid19Controller::class, "edit"]);
+Route::get('/covid19', [Covid19Controller::class, 'index']);
+Route::get('/covid19/{id}', [Covid19Controller::class, 'show']);
+Route::get('/covid19 /{id}', [Covid19Controller::class, 'store']);
+Route::get('/covid19 /{id}', [Covid19Controller::class, 'update']);
+Route::delete('/covid19/{id}', [Covid19Controller::class, 'destroy']);
 
-Route::get('/covid19/{id}',[ Covid19Controller::class,'show' ]);
 
-Route::get('/covid19 /{id}',[ Covid19Controller::class,'store' ]);
+Route::get("/staff/create", [StaffController::class, "create"]);
+Route::get("/staff/{id}/edit", [StaffController::class, "edit"]);
+Route::get('/staff', [StaffController::class, "index"]);
+Route::get('/staff/{id}', [StaffController::class, "show"]);
+Route::post("/staff", [StaffController::class, "store"]);
+Route::patch("/staff/{id}", [StaffController::class, "update"]);
+Route::delete('/staff/{id}', [StaffController::class, 'destroy']);
 
-Route::get('/covid19 /{id}',[ Covid19Controller::class,'update' ]);
-
-Route::delete('/covid19/{id}', [ Covid19Controller::class , 'destroy' ]);
-
-use App\Http\Controllers\StaffController;
-Route::get("/staff/create",[ StaffController::class , "create" ]);
-Route::get("/staff/{id}/edit", [ StaffController::class , "edit" ]);
-Route::get('/staff', [ StaffController::class,"index" ]);
-Route::get('/staff/{id}', [ StaffController::class,"show" ]);
-Route::post("/staff",[ StaffController::class , "store" ]);
-Route::patch("/staff/{id}", [ StaffController::class , "update" ]);
-Route::delete('/staff/{id}', [ StaffController::class , 'destroy' ]);
-
-use App\Http\Controllers\PostController;
 //Route::resource('post', 'PostController');
-Route::resource('post', PostController :: class);
+Route::resource('post', PostController::class);
